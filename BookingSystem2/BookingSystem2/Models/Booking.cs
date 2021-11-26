7a4d3053f38ce768d6335481958442f9b5b278c2
ThomasHenriksen11 <61276429+ThomasHenriksen11@users.noreply.github.com>
2021-11-26 10:03:17 +0100

    Classes inserted
    Klasser fra DCD indsat

---
+++ BookingSystem2/BookingSystem2/Models/Booking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingSystem2.Models
+{
+    public class Booking
+    {
+        public int bookingId { get; set; }
+        public DateTime paymentDate { get; set; }
+        public bool status { get; set; }
+        public DateTime date { get; set; }
+        public DateTime startTime { get; set; }
+        public float duration { get; set; }
+        public string topic { get; set; }
+        public string comment { get; set; }
+    }
+}
---
+++ BookingSystem2/BookingSystem2/Models/TimeSlot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingSystem2.Models
+{
+    public class TimeSlot
+    {
+        public int timeSlotId { get; set; }
+        public DateTime date { get; set; }
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
+        public float stanmdardPrice { get; set; }
+        public float specialPrice { get; set; }
+    }
+}
