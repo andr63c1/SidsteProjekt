0c1615b6b7f1a5e41619a79e9962068b100c9882
ThomasHenriksen11 <61276429+ThomasHenriksen11@users.noreply.github.com>
2021-12-08 09:20:37 +0100

    Tab HTML

---
+++ BookingSystem3/BookingSystem3/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingSystem3.Controllers
+{
+    [Route("bookinger")]
+    public class AdminController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        [Route("~/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
