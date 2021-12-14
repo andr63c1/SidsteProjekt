1dde96726caa0399730393ad29d07cb4ce361084
Andreas <andr63c1@edu.ucl.dk>
2021-12-14 19:30:54 +0100

    Database migations Role

---
+++ BookingSystem3/BookingSystem3/Data/Migrations/20211214173027_ApplicationUserAddRoles1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingSystem3.Data.Migrations
+{
+    public partial class ApplicationUserAddRoles1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
