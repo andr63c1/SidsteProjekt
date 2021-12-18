49a8a84d1a3cf2bb853a395af367234f5ab545e1
Andreas <andr63c1@edu.ucl.dk>
2021-12-18 13:12:21 +0100

    AdminUpdate

---
+++ BookingSystem3/BookingSystem3/Models/BookingPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingSystem3.Models
+{
+    public class BookingPageViewModel
+    {
+        public BookingVM Booking { get; set; }
+        public IEnumerable<TimeSlot> TimeSlots { get; set; }
+    }
+}
